76ee991e40e7ec5aadd22f5bcb6cf2443e067faf
Michał Konrad Rusinek <rusinekmk@gmail.com>
2015-02-23 18:26:14 +0100

    Changed parsing methods to eval based ones, due to diffrences of classes (or their lack) when parsing different pages.

--- app/models/currency_site.rb
+++ app/models/currency_site.rb
@@ -8,13 +8,29 @@ class CurrencySite < ActiveRecord::Base
   belongs_to :site 
 
   def parse_buy_value
-    page = Nokogiri::HTML(open(site.url))   
-    page.css(currency_class).css(buy_parsing_class).text.gsub(",",".").to_f
+    set_buy_parsing_method
+    execute_parsing_method
+    # page.css(currency_class).css(buy_parsing_class).text.gsub(",",".").to_f
   end
 
-  def parse_sell_value
-    page = Nokogiri::HTML(open(site.url))   
-    page.css(currency_class).css(sell_parsing_class).text.gsub(",",".").to_f
+  def parse_sell_value   
+    set_buy_parsing_method
+    execute_parsing_method
+    # page.css(currency_class).css(sell_parsing_class).text.gsub(",",".").to_f
+  end
+
+  private
+
+  def set_buy_parsing_method
+    eval("def execute_parsing_method; page = Nokogiri(open(site.url)); #{buy_parsing_class}; end;")
+  end
+
+  def set_sell_parsing_method
+    eval("def execute_parsing_method; page = Nokogiri(open(site.url)); #{sell_parsing_class}; end;")
+  end
+
+  def execute_parsing_method
+    # code here is written by eval
   end
 end
  
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@ chf = Currency.create(name: "Swiss Frank")
 usd = Currency.create(name: "US Dollar")
 
 # seeds for kantor_internetowy site
-CurrencySite.create(site_id: kantor_internetowy.id, currency_id: eur.id, currency_class: "#waluta-EUR", buy_parsing_class:".kurs_kupna", sell_parsing_class:".kurs_sprzedazy")
-CurrencySite.create(site_id: kantor_internetowy.id, currency_id: chf.id, currency_class: "#waluta-CHF", buy_parsing_class:".kurs_kupna", sell_parsing_class:".kurs_sprzedazy")
-CurrencySite.create(site_id: kantor_internetowy.id, currency_id: usd.id, currency_class: "#waluta-USD", buy_parsing_class:".kurs_kupna", sell_parsing_class:".kurs_sprzedazy")
+CurrencySite.create(site_id: kantor_internetowy.id, currency_id: eur.id, currency_class: "#waluta-EUR", buy_parsing_class:"page.css('#waluta-EUR').css('.kurs_kupna').text.gsub(',','.').to_f", sell_parsing_class:"page.css('#waluta-EUR').css('.kurs_sprzedazy').text.gsub(',','.').to_f")
+CurrencySite.create(site_id: kantor_internetowy.id, currency_id: chf.id, currency_class: "#waluta-CHF", buy_parsing_class:"page.css('#waluta-CHF').css('.kurs_kupna').text.gsub(',','.').to_f", sell_parsing_class:"page.css('#waluta-CHF').css('.kurs_sprzedazy').text.gsub(',','.').to_f")
+CurrencySite.create(site_id: kantor_internetowy.id, currency_id: usd.id, currency_class: "#waluta-USD", buy_parsing_class:"page.css('#waluta-USD').css('.kurs_kupna').text.gsub(',','.').to_f", sell_parsing_class:"page.css('#waluta-USD').css('.kurs_sprzedazy').text.gsub(',','.').to_f")
 
 # seeds for kantor_online site
 # CurrencySite.create(site_id: kantor_online.id, currency_id: eur.id, currency_class: "#eur", buy_parsing_class:"#eurbuy", sell_parsing_class:"#eursell")
