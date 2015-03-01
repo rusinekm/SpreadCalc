3c4af8df381d95bf65efdab00c809424d3a1b586
Michał Konrad Rusinek <rusinekmk@gmail.com>
2015-03-02 00:44:20 +0100

    changed parsing method

--- config/initializers/parsing_pages_to_increase_database.rb
+++ config/initializers/parsing_pages_to_increase_database.rb
@@ -6,16 +6,16 @@ StatNode.establish_connection
 
 def statnode_creator
   Site.all.each do |current_site| 
-    parsing_page = Thread.new do
+    # parsing_page = Thread.new do
       page = Nokogiri(open(current_site.url))
         current_site.currency_sites.each do |current_currency_site|
           buy_value = current_currency_site.parse_buy_value(page)
           sell_value = current_currency_site.parse_sell_value(page)
           StatNode.create(currency_id: current_currency_site.currency_id, site_id: current_currency_site.site_id, buy_value: buy_value, sell_value: sell_value)  
       end
-    end
+    # end
   end
-  
+
  find_average_values
  set_best_values
 end
@@ -29,14 +29,13 @@ def set_best_values
 end
   
 background_currency_data_parsing = Thread.new do 
-  # sleep 55.seconds #used to wait for the server to start
   if (StatNode.last && (Time.now - StatNode.last.created_at) <= 5.minutes) then
     sleep (5.minutes - (Time.now - StatNode.last.created_at))
   end
   while true do
-    thread_collecting_website_data = Thread.new do
+    # thread_collecting_website_data = Thread.new do
       statnode_creator
-    end
+    # end
     sleep 5.minutes
   end
 end
